a428a131642c8b09417871990f165b091667d0f1
anagornii <ana.nagornii@endava.com>
2020-01-14 17:52:06 +0200

    wip on creating first page

--- src/main/java/com/endava/anagornaia/demoblog/persistence/model/CommentEntity.java
+++ src/main/java/com/endava/anagornaia/demoblog/persistence/model/CommentEntity.java
@@ -4,7 +4,6 @@ import lombok.Data;
 
 import javax.persistence.*;
 import javax.validation.constraints.Email;
-import java.util.Set;
 
 @Data
 @Entity
@@ -32,6 +31,4 @@ public class CommentEntity {
     @ManyToOne
     @JoinColumn(name = "blog_post_id")
     BlogPostEntity blogPost;
-
-
 }
